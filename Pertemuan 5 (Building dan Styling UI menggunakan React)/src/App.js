cfe1009c5eacfba01a91e5aa96e87f18b065865b
Ardelingga Pramesta Kusuma <ardelingga.pk@gmail.com>
2022-04-02 10:03:38 +0700

    Push hasil praktek Pertemuan 5 & Pertemuan 6

---
+++ Pertemuan 5 (Building dan Styling UI menggunakan React)/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import Hero from "./components/Hero/Hero"
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
